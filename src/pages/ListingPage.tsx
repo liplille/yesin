b453018150f698780ea36e5b2b3074c803cdf4c4
Joey <301@gmx.fr>
2026-01-18 12:21:20 +0100

    link yesin presence

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -18,7 +18,7 @@ import {
 // Branding
 // --------------------
 const PRESENCE_NAME = "YesIn Presence™";
-const VOICE_NAME = "YesIn Radio";
+const VOICE_NAME = "YesIn Radio™";
 const PRESENCE_LABEL = "Publicité IA";
 
 // --------------------
